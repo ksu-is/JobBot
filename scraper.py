7e4b19951b0b1456e464f3ab524c8ded5d5c2dcb
mhaile-1 <69773829+mhaile-1@users.noreply.github.com>
2020-11-20 21:32:03 -0500

    update scraper.py to add linkedin information
    using the same formatting, adding Linkedin location to parse from and update code

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 from decouple import config, Csv
-from boards import indeed, monster
+from boards import indeed, monster , linkedin
 
 monster_url = config('MONSTER_URL')
 indeed_url = config('INDEED_URL')
+linkedin_url = config('LINKEDIN_URL')
 disgard_list = config('DESCRIPTION_DOESNT_CONTAIN', cast=Csv())
 
 print('Getting jobs from monster.com')
@@ -15,7 +16,12 @@ indeed = indeed.IndeedJobs(indeed_url)
 indeed_jobs = indeed.get()
 print(f'Found {len(monster_jobs)} job listings within the specified criteria')
 
-unfiltered_jobs = monster_jobs + indeed_jobs
+print('Getting jobs from linkedin.com')
+linkedin = linkedin.LinkedinJobs(linkedin_url)
+linkedin_jobs = linkedin.get()
+print(f'Found {len(linkedin_jobs)} job listings within the specified criiteria')
+
+unfiltered_jobs = monster_jobs + indeed_jobs + linkedin_jobs
 filtered_jobs = []
 
 for job in unfiltered_jobs:
